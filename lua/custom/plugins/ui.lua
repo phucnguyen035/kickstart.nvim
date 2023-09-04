c3ce509ea71e097aa6855e985b215c7efdde1d25
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-09-04 21:12:56 +0700

    refactor: disable black background for theme

--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -7,11 +7,11 @@ return {
     init = function()
       require("catppuccin").setup({
         color_overrides = {
-          mocha = {
-            base = '#000000',
-            mantle = '#000000',
-            crust = '#000000',
-          },
+          -- mocha = {
+          --   base = '#000000',
+          --   mantle = '#000000',
+          --   crust = '#000000',
+          -- },
         },
         integrations = {
           harpoon = true,
