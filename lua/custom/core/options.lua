6f89d6cbf39c238a14e31b2153d9daa0209a8707
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-09-12 09:21:26 +0700

    feat: add border to floating windows

--- lua/custom/core/options.lua
+++ lua/custom/core/options.lua
@@ -38,3 +38,20 @@ vim.o.foldenable = true
 
 vim.o.shiftwidth = 2
 vim.o.tabstop = 2
+
+local _border = "single"
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+	vim.lsp.handlers.hover, {
+		border = _border
+	}
+)
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+	vim.lsp.handlers.signature_help, {
+		border = _border
+	}
+)
+
+vim.diagnostic.config {
+	float = { border = _border }
+}
