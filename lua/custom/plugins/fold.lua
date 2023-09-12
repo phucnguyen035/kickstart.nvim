4daee7184c08b0b5353bda8fd1be6d2b54dde72a
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-09-12 09:23:56 +0700

    refactor: remove fold virtual text
    already included in catppuccin

--- lua/custom/plugins/fold.lua
+++ lua/custom/plugins/fold.lua
@@ -4,33 +4,6 @@ return {
 	event = "BufRead",
 	opts = {
 		close_fold_kinds = { 'imports', 'comment' },
-		fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
-			local newVirtText = {}
-			local suffix = (' 󰁂 %d '):format(endLnum - lnum)
-			local sufWidth = vim.fn.strdisplaywidth(suffix)
-			local targetWidth = width - sufWidth
-			local curWidth = 0
-			for _, chunk in ipairs(virtText) do
-				local chunkText = chunk[1]
-				local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-				if targetWidth > curWidth + chunkWidth then
-					table.insert(newVirtText, chunk)
-				else
-					chunkText = truncate(chunkText, targetWidth - curWidth)
-					local hlGroup = chunk[2]
-					table.insert(newVirtText, { chunkText, hlGroup })
-					chunkWidth = vim.fn.strdisplaywidth(chunkText)
-					-- str width returned from truncate() may less than 2nd argument, need padding
-					if curWidth + chunkWidth < targetWidth then
-						suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
-					end
-					break
-				end
-				curWidth = curWidth + chunkWidth
-			end
-			table.insert(newVirtText, { suffix, 'MoreMsg' })
-			return newVirtText
-		end
 	},
 	keys = {
 		{ 'zr', '<cmd>lua require("ufo").openFoldsExceptKinds()<cr>', desc = 'Open Folds Except Kinds' },
@@ -44,7 +17,7 @@ return {
 				if not winid then
 					-- choose one of coc.nvim and nvim lsp
 					vim.fn.CocActionAsync('definitionHover') -- coc.nvim
-					vim.lsp.buf.hover()
+					vim.lsp.buf.hover({})
 				end
 			end,
 			desc = 'Hover'
