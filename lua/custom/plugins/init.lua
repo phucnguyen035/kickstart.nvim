6294bd3d63ed0e5497cae2986da43ad9249ea6c0
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-09-15 03:49:31 +0700

    feat: add format commands

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,4 +1,5 @@
--- You can add your own plugins here or in other files in this directory!
+---@diagnostic disable: inject-field
+
 return {
   {
     'ThePrimeagen/harpoon',
@@ -37,6 +38,40 @@ return {
     'stevearc/conform.nvim',
     event = 'BufRead',
     cond = not vim.g.vscode,
+    config = function(_, opts)
+      local conform = require 'conform'
+      conform.setup(opts)
+
+      vim.api.nvim_create_user_command('Format', function(args)
+        local range = nil
+        if args.count ~= -1 then
+          local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+          range = {
+            start = { args.line1, 0 },
+            ['end'] = { args.line2, end_line:len() },
+          }
+        end
+        conform.format { async = true, lsp_fallback = true, range = range }
+      end, { range = true })
+
+      vim.api.nvim_create_user_command('FormatDisable', function(args)
+        if args.bang then
+          -- FormatDisable! will disable formatting just for this buffer
+          vim.b.disable_autoformat = true
+        else
+          vim.g.disable_autoformat = true
+        end
+      end, {
+        desc = 'Disable autoformat-on-save',
+        bang = true,
+      })
+      vim.api.nvim_create_user_command('FormatEnable', function()
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+      end, {
+        desc = 'Re-enable autoformat-on-save',
+      })
+    end,
     opts = {
       formatters_by_ft = {
         lua = { 'stylua' },
@@ -51,10 +86,14 @@ return {
         gohtmltmpl = { 'prettierd' },
         json = { 'jq', 'prettierd' },
       },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+
+        return { timeout_ms = 500, lsp_fallback = true }
+      end,
       formatters = {
         blackd = {
           command = 'blackd-client',
