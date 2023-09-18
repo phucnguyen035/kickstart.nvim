780af0836617f5a3609656df60ea3e4cba82fe0a
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2023-09-18 11:42:52 +0700

    feat: add move window keys

--- lua/custom/core/keymaps.lua
+++ lua/custom/core/keymaps.lua
@@ -11,6 +11,13 @@ map({ 'n', 'v' }, '<Space>', '<Nop>', { noremap = true })
 -- Remap for dealing with word wrap
 map('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 map('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Move to window using the <ctrl> hjkl keys
+map('n', '<C-h>', '<C-w>h', { desc = 'Go to left window', remap = true })
+map('n', '<C-j>', '<C-w>j', { desc = 'Go to lower window', remap = true })
+map('n', '<C-k>', '<C-w>k', { desc = 'Go to upper window', remap = true })
+map('n', '<C-l>', '<C-w>l', { desc = 'Go to right window', remap = true })
+
 -- Diagnostic keymaps
 map('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 map('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
