2accfd814c93ee4a51d6619ce39f2298767992ca
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2024-04-15 16:40:27 +0700

    update fold config

--- lua/custom/plugins/fold.lua
+++ lua/custom/plugins/fold.lua
@@ -1,25 +1,47 @@
+local ftMap = {
+  vim = 'indent',
+  python = { 'indent' },
+  git = '',
+}
+
 return {
   'kevinhwang91/nvim-ufo',
   cond = not vim.g.vscode,
   dependencies = 'kevinhwang91/promise-async',
   event = 'BufRead',
-  opts = {
-    close_fold_kinds = { 'imports', 'comment' },
-  },
-  keys = {
-    { 'zr', '<cmd>lua require("ufo").openFoldsExceptKinds()<cr>', desc = 'Open Folds Except Kinds' },
-    { 'zR', '<cmd>lua require("ufo").openAllFolds()<cr>' },
-    { 'zM', '<cmd>lua require("ufo").closeFoldsWith()<cr>', desc = 'yikes' },
-    { 'zM', '<cmd>lua require("ufo").closeAllFolds()<cr>' },
-    {
-      'K',
-      function()
-        local winid = require('ufo').peekFoldedLinesUnderCursor()
-        if not winid then
-          vim.lsp.buf.hover()
-        end
+  config = function()
+    require('ufo').setup {
+      open_fold_hl_timeout = 150,
+      close_fold_kinds_for_ft = {
+        default = { 'imports', 'comment' },
+      },
+      preview = {
+        win_config = {
+          border = { '', '─', '', '', '', '─', '', '' },
+          winhighlight = 'Normal:Folded',
+          winblend = 0,
+        },
+        mappings = {
+          scrollU = '<C-u>',
+          scrollD = '<C-d>',
+          jumpTop = '[',
+          jumpBot = ']',
+        },
+      },
+      provider_selector = function(bufnr, filetype, buftype)
+        return ftMap[filetype] or { 'treesitter', 'indent' }
       end,
-      desc = 'Hover',
-    },
-  },
+    }
+
+    vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+    vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+    vim.keymap.set('n', 'zr', require('ufo').openFoldsExceptKinds)
+    vim.keymap.set('n', 'zm', require('ufo').closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
+    vim.keymap.set('n', 'zk', function()
+      local winid = require('ufo').peekFoldedLinesUnderCursor()
+      if not winid then
+        vim.lsp.buf.hover()
+      end
+    end)
+  end,
 }
