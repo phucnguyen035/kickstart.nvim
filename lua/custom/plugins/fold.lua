01856023f2e70b7c639b11cf62c019d47f920e57
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2024-04-15 16:57:53 +0700

    add custom fold selector

--- lua/custom/plugins/fold.lua
+++ lua/custom/plugins/fold.lua
@@ -4,6 +4,28 @@ local ftMap = {
   git = '',
 }
 
+-- lsp -> treesitter -> indent
+---@param bufnr number
+---@return Promise
+local function customizeSelector(bufnr)
+  local function handleFallbackException(err, providerName)
+    if type(err) == 'string' and err:match 'UfoFallbackException' then
+      return require('ufo').getFolds(bufnr, providerName)
+    else
+      return require('promise').reject(err)
+    end
+  end
+
+  return require('ufo')
+    .getFolds(bufnr, 'lsp')
+    :catch(function(err)
+      return handleFallbackException(err, 'treesitter')
+    end)
+    :catch(function(err)
+      return handleFallbackException(err, 'indent')
+    end)
+end
+
 return {
   'kevinhwang91/nvim-ufo',
   cond = not vim.g.vscode,
@@ -29,7 +51,7 @@ return {
         },
       },
       provider_selector = function(bufnr, filetype, buftype)
-        return ftMap[filetype] or { 'treesitter', 'indent' }
+        return ftMap[filetype] or customizeSelector
       end,
     }
 
