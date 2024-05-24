7a449fd01a5680104e46afa60aef796989fbb30a
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2024-05-24 17:24:22 +0700

    replace nvim tmux navigation with smart splits

--- lua/custom/plugins/tmux.lua
+++ lua/custom/plugins/tmux.lua
@@ -1,23 +1,31 @@
+local nmap = function(key, cmd, opts)
+  vim.keymap.set('n', key, cmd, opts)
+end
+
 return {
   {
-    'alexghergh/nvim-tmux-navigation',
-    config = function()
-      local nvim_tmux_nav = require 'nvim-tmux-navigation'
+    'mrjones2014/smart-splits.nvim',
+    lazy = false,
+    opts = {},
+    config = function(_, opts)
+      local smart_splits = require 'smart-splits'
+      smart_splits.setup(opts)
 
-      nvim_tmux_nav.setup {
-        disable_when_zoomed = true, -- defaults to false
-      }
+      nmap('<C-h>', smart_splits.move_cursor_left)
+      nmap('<C-j>', smart_splits.move_cursor_down)
+      nmap('<C-k>', smart_splits.move_cursor_up)
+      nmap('<C-l>', smart_splits.move_cursor_right)
+      nmap('<C-\\>', smart_splits.move_cursor_previous)
 
-      local nmap = function(key, cmd)
-        vim.keymap.set('n', key, cmd)
-      end
+      nmap('<A-h>', smart_splits.resize_left)
+      nmap('<A-j>', smart_splits.resize_down)
+      nmap('<A-k>', smart_splits.resize_up)
+      nmap('<A-l>', smart_splits.resize_right)
 
-      nmap('<C-h>', nvim_tmux_nav.NvimTmuxNavigateLeft)
-      nmap('<C-j>', nvim_tmux_nav.NvimTmuxNavigateDown)
-      nmap('<C-k>', nvim_tmux_nav.NvimTmuxNavigateUp)
-      nmap('<C-l>', nvim_tmux_nav.NvimTmuxNavigateRight)
-      nmap('<C-\\>', nvim_tmux_nav.NvimTmuxNavigateLastActive)
-      nmap('<C-Space>', nvim_tmux_nav.NvimTmuxNavigateNext)
+      nmap('<leader>bh', smart_splits.swap_buf_left, { desc = 'Swap buffer left' })
+      nmap('<leader>bj', smart_splits.swap_buf_down, { desc = 'Swap buffer down' })
+      nmap('<leader>bk', smart_splits.swap_buf_up, { desc = 'Swap buffer up' })
+      nmap('<leader>bl', smart_splits.swap_buf_right, { desc = 'Swap buffer right' })
     end,
   },
 }
