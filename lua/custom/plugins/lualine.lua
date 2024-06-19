c79aaf6556c4514959bccdf80d5114ba2b66ea62
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2024-06-19 23:55:48 +0700

    simplify lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,41 +1,3 @@
-local mode_map = {
-  ['n'] = 'N',
-  ['no'] = 'O·P',
-  ['nov'] = 'O·P',
-  ['noV'] = 'O·P',
-  ['no\22'] = 'O·P',
-  ['niI'] = 'N·I',
-  ['niR'] = 'N·R',
-  ['niV'] = 'N',
-  ['nt'] = 'N·T',
-  ['v'] = 'V',
-  ['vs'] = 'V',
-  ['V'] = 'V·L',
-  ['Vs'] = 'V·L',
-  ['\22'] = 'V·B',
-  ['\22s'] = 'V·B',
-  ['s'] = 'S',
-  ['S'] = 'S·L',
-  ['\19'] = 'S·B',
-  ['i'] = 'I',
-  ['ic'] = 'I·C',
-  ['ix'] = 'I·X',
-  ['R'] = 'R',
-  ['Rc'] = 'R·C',
-  ['Rx'] = 'R·X',
-  ['Rv'] = 'V·R',
-  ['Rvc'] = 'RVC',
-  ['Rvx'] = 'RVX',
-  ['c'] = 'C',
-  ['cv'] = 'EX',
-  ['ce'] = 'EX',
-  ['r'] = 'R',
-  ['rm'] = 'M',
-  ['r?'] = 'C',
-  ['!'] = 'SH',
-  ['t'] = 'T',
-}
-
 return {
   {
     -- Set lualine as statusline
@@ -56,17 +18,17 @@ return {
       },
       sections = {
         lualine_a = {
-          { 'mode', separator = { left = '' }, right_padding = 10 },
-        },
-
-        lualine_b = {
-          { 'filename', path = 0 },
           'branch',
         },
 
-        lualine_c = {
-          { 'diff', colored = true, symbols = { added = ' ', modified = ' ', removed = ' ' } },
+        lualine_b = {
           'diagnostics',
+        },
+
+        lualine_c = {
+          'diff',
+          '%=',
+          { 'filename', path = 1, shorting_target = 10 },
         },
 
         lualine_x = { 'copilot' },
