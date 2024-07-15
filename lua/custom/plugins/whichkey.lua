f2f828d6c55faff1a278e97279c411a911986fa6
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2024-07-15 14:53:52 +0700

    upgrade which key to v3

--- lua/custom/plugins/whichkey.lua
+++ lua/custom/plugins/whichkey.lua
@@ -2,49 +2,37 @@ return {
   'folke/which-key.nvim',
   cond = not vim.g.vscode,
   event = 'VeryLazy',
-  config = function()
-    local wk = require 'which-key'
-
-    wk.register {
-      ['<leader>c'] = {
-        name = '+code/copilot',
-        ['c'] = {
-          name = '+chat',
-        },
-        f = { '<cmd>Format<cr>', 'Format code', mode = { 'n', 'v' } },
-        t = {
-          name = '+toggle',
-          f = { '<cmd>FormatToggle<cr>', 'Toggle format (global)' },
-          F = { '<cmd>FormatToggle!<cr>', 'Toggle format (buffer)' },
-        },
-      },
-      ['<leader>h'] = {
-        name = '+harpoon',
-      },
-      ['<leader>f'] = {
-        name = '+file',
-      },
-      ['<leader>s'] = {
-        name = '+search',
-      },
-      ['<leader>g'] = {
-        name = '+git',
-      },
-      ['<leader>b'] = {
-        name = '+buffer',
-        b = { '<cmd>b#<cr>', 'Switch to last buffer' },
-        c = { '<cmd>WipeWindowlessBufs<cr>', 'Clear all buffers, keep current', { silent = true } },
-      },
-      ['<leader>x'] = {
-        name = '+trouble',
-      },
-      ['<leader>t'] = {
-        name = '+tab',
-        g = { '<cmd>tabfirst<cr>', 'Go to first tab' },
-        G = { '<cmd>tablast<cr>', 'Go to last tab' },
-        n = { '<cmd>tabnew<cr>', 'New tab' },
-        c = { '<cmd>tabclose<cr>', 'Close tab' },
-      },
-    }
-  end,
+  opts = {
+    spec = {
+      { '<leader>q', group = 'quit' },
+      { '<leader>c', group = 'code/copilot' },
+      { '<leader>cc', group = 'chat', icon = ' ' },
+      { '<leader>ct', group = 'toggle' },
+      { '<leader>f', group = 'file' },
+      { '<leader>s', group = 'search' },
+      { '<leader>g', group = 'git' },
+      { '<leader>b', group = 'buffer' },
+      { '<leader>x', group = 'trouble' },
+      { '<leader>t', group = 'tab' },
+      { '<leader>w', group = 'workspace' },
+      { '<leader>cf', '<cmd>Format<cr>', desc = 'Format code', mode = { 'n', 'v' } },
+      { '<leader>ctf', '<cmd>FormatToggle<cr>', desc = 'Toggle format (global)' },
+      { '<leader>ctF', '<cmd>FormatToggle!<cr>', desc = 'Toggle format (buffer)' },
+      { '<leader>bb', '<cmd>b#<cr>', desc = 'Switch to last buffer' },
+      { '<leader>bc', '<cmd>WipeWindowlessBufs<cr>', desc = 'Clear all buffers, keep current', silent = true },
+      { '<leader>tg', '<cmd>tabfirst<cr>', desc = 'Go to first tab' },
+      { '<leader>tG', '<cmd>tablast<cr>', desc = 'Go to last tab' },
+      { '<leader>tn', '<cmd>tabnew<cr>', desc = 'New tab' },
+      { '<leader>tx', '<cmd>tabclose<cr>', desc = 'Remove tab' },
+    },
+  },
+  keys = {
+    {
+      '<leader>?',
+      function()
+        require('which-key').show { global = false }
+      end,
+      desc = 'Buffer Local Keymaps (which-key)',
+    },
+  },
 }
